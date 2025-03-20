ee407f3abafa687bd7ee49c3b334d89b76070ce5
Seonggyu-Min <minsunggu705@gmail.com>
2025-03-20 17:46:43 +0900

    [Bronze V] Title: 나머지, Time: 48 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cs
@@ -0,0 +1,14 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+        int c = int.Parse(input[2]);
+
+        Console.WriteLine((a + b)% c);
+        Console.WriteLine(((a % c) + (b % c)) % c);
+        Console.WriteLine((a * b) % c);
+        Console.WriteLine(((a % c) * (b % c)) % c);        }
+}
