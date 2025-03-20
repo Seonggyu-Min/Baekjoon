e8279986597176694449f3eb94ed7dc2b60c6ab7
Seonggyu-Min <minsunggu705@gmail.com>
2025-03-20 17:33:44 +0900

    [Bronze V] Title: A/B, Time: 48 ms, Memory: 5516 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+
+        double A = double.Parse(input[0]);
+        double B = double.Parse(input[1]);
+
+        Console.WriteLine(A / B);
+    }
+}
