325effc727a22823bedc2785c032a9d51ee06833
Seonggyu-Min <minsunggu705@gmail.com>
2025-03-21 17:59:44 +0900

    [Bronze V] Title: 고양이, Time: 44 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,10 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\\    /\\");
+        Console.WriteLine(" )  ( \')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(" \\(__)|");
+    }
+}
