062e21638eb910656c4d16daaa58746d420c8b75
Seonggyu-Min <minsunggu705@gmail.com>
2025-03-23 13:43:40 +0900

    [Bronze V] Title: A+B - 7, Time: 56 ms, Memory: 6072 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cs
@@ -0,0 +1,17 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string caseNum = Console.ReadLine();
+        int intCaseNum = int.Parse(caseNum);
+
+        for (int i = 0; i < intCaseNum; i++)
+        {
+            string[] plusNum = Console.ReadLine().Split();
+            int num1 = int.Parse(plusNum[0]);
+            int num2 = int.Parse(plusNum[1]);
+
+            System.Console.WriteLine($"Case #{i + 1}: {num1 + num2}");
+        }
+    }
+}
