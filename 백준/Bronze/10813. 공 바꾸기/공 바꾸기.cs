1fbd90279c188ef56df6d834ee30703e668fb5aa
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-03 00:18:12 +0900

    [Bronze II] Title: 공 바꾸기, Time: 60 ms, Memory: 6028 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cs
@@ -0,0 +1,30 @@
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int[] infor = input.Select(int.Parse).ToArray();
+
+        // 바구니
+        int[] bask = new int[infor[0]];
+        for (int i = 0; i < infor[0]; i++)
+        {
+            bask[i] = i + 1;
+        }
+
+        // 바구니 안의 공 교체
+        for (int i = 0; i < infor[1]; i++)
+        {
+            string[] strChange = Console.ReadLine().Split();
+            int[] chg = strChange.Select(int.Parse).ToArray();
+
+            int temp = bask[chg[0] - 1];
+            bask[chg[0] - 1] = bask[chg[1] - 1];
+            bask[chg[1] - 1] = temp;
+        }
+
+        for (int i = 0; i < bask.Length; i++)
+            Console.Write($"{bask[i]} ");
+        
+    }
+}
