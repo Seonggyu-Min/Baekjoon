e778296d06c12fe98a2c231c49aff6a91dcf7be9
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-03 20:49:03 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 56 ms, Memory: 5956 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] strInput = Console.ReadLine().Split();
+        int[] input = strInput.Select(int.Parse).ToArray();
+
+        int[] basket = new int[input[0]];
+
+        for (int i = 0; i < input[0]; i++)
+        {
+            basket[i] = i + 1;
+        }
+
+        for (int i = 0; i < input[1]; i++)
+        {
+            string[] s = Console.ReadLine().Split();
+            int[] trade = s.Select(int.Parse).ToArray();
+
+            Array.Reverse(basket, trade[0] - 1, trade[1] - trade[0] + 1);
+        }
+
+        foreach (int i in basket)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
