ee1d17484a793d58ecda71522c81c9c8ac78352d
Seonggyu-Min <minsunggu705@gmail.com>
2025-04-15 00:37:03 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 48 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+        }
+    }
+}
