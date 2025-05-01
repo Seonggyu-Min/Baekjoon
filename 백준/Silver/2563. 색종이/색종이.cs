c2cfea75bfbac05a80fcd8586c824eadd5381817
Seonggyu-Min <minsunggu705@gmail.com>
2025-05-02 02:24:17 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 5792 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cs
+++ 백준/Silver/2563. 색종이/색종이.cs
@@ -1,39 +1,30 @@
-using System.Numerics;
-
 public class Program
 {
     static void Main(string[] args)
     {
-        HashSet<Vector2> set = new HashSet<Vector2>();
+        bool[,] paper = new bool[100, 100];
 
-        int num = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
 
-        for (int i = 0; i < num; i++)
+        for (int i = 0; i < n; i++)
         {
-            string input = Console.ReadLine();
-            string[] dist = input.Split();
+            string[] input = Console.ReadLine().Split();
+            int x = int.Parse(input[0]);
+            int y = int.Parse(input[1]);
 
-            int leftDist = int.Parse(dist[0]);
-            int downDist = int.Parse(dist[1]);
-
-            for (int j = leftDist; j < leftDist + 10; j++)
+            for (int dx = 0; dx < 10; dx++)
             {
-                for (int k = downDist; k < downDist + 10; k++)
+                for (int dy = 0; dy < 10; dy++)
                 {
-                    Vector2 vector2 = new Vector2();
-                    vector2.X = j;
-                    vector2.Y = k;
-                    set.Add(vector2);
+                    paper[x + dx, y + dy] = true;
                 }
             }
         }
 
         int count = 0;
-
-        foreach (Vector2 vector in set)
+        foreach (bool covered in paper)
         {
-            set.Remove(vector);
-            count++;
+            if (covered) count++;
         }
 
         Console.WriteLine(count);
