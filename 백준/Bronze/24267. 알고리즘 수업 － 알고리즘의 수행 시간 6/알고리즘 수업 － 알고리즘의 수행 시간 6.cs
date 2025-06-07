e43b34610f0396d957347b07ebbaa2baa1ea96c6
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-07 14:50:06 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 48 ms, Memory: 5500 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.cs
@@ -0,0 +1,11 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        long num = int.Parse(Console.ReadLine());
+        long sum = (num - 2) * (num - 1) * num / 6;
+
+        Console.WriteLine(sum);
+        Console.WriteLine(3);
+    }
+}
