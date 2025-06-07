a7d3cf79301d0db10f37b2202a4049437a2ac432
Seonggyu-Min <minsunggu705@gmail.com>
2025-06-07 14:47:37 +0900

    [Bronze V] Title: 직사각형, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cs
@@ -0,0 +1,10 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(a * b);
+    }
+}
