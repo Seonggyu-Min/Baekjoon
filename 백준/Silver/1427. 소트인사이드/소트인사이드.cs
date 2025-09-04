90aaa58a1e6f9bd17d2c716f6c639995633165a3
Seonggyu-Min <minsunggu705@gmail.com>
2025-09-05 01:22:13 +0900

    [Silver V] Title: 소트인사이드, Time: 48 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cs
@@ -0,0 +1,23 @@
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        char[] chars = input.ToCharArray();
+
+        for (int i = 0; i < chars.Length - 1; i++)
+        {
+            for (int j = 0; j < chars.Length - i - 1; j++)
+            {
+                if (chars[j] < chars[j + 1])
+                {
+                    char temp = chars[j];
+                    chars[j] = chars[j + 1];
+                    chars[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine(chars);
+    }
+}
