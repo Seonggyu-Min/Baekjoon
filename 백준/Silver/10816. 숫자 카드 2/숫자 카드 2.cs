da5acab7a07fdad39a22871849bc27c975b5350d
Seonggyu-Min <minsunggu705@gmail.com>
2025-12-15 16:54:56 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 740 ms, Memory: 100308 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
@@ -0,0 +1,45 @@
+using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int ownedCardCount = int.Parse(Console.ReadLine());
+
+        Dictionary<int, int> ownedCardDict = new();
+
+        string[] ownedCardArr = Console.ReadLine().Split();
+        for (int i = 0; i < ownedCardArr.Length; i++)
+        {
+            int card = int.Parse(ownedCardArr[i]);
+
+            if (ownedCardDict.TryGetValue(card, out int count))
+            {
+                ownedCardDict[card] = count + 1;
+            }
+            else
+            {
+                ownedCardDict[card] = 1;
+            }
+        }
+
+        int checkCardCount = int.Parse(Console.ReadLine());
+        StringBuilder sb = new();
+
+        string[] checkCardArr = Console.ReadLine().Split();
+        for (int i = 0; i < checkCardArr.Length; i++)
+        {
+            int card = int.Parse(checkCardArr[i]);
+
+            if (ownedCardDict.TryGetValue(card, out int count))
+                sb.Append(count);
+            else
+                sb.Append('0');
+
+            if (i < checkCardCount - 1)
+                sb.Append(' ');
+        }
+
+        Console.Write(sb);
+    }
+}
