c78e5a23834138a384b4142f3a56d2f9e9dae7ee
Seonggyu-Min <minsunggu705@gmail.com>
2026-07-10 00:50:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.18 ms, Memory: 46.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -0,0 +1,15 @@
+using System.Text;
+
+public class Solution {
+    public string solution(string phone_number) {
+        var sb = new StringBuilder(phone_number.Length);
+
+        for (int i = 0; i < phone_number.Length; i++)
+        {
+            if (i < phone_number.Length - 4) sb.Append('*');
+            else sb.Append(phone_number[i]);
+        }
+
+        return sb.ToString();
+    }
+}
