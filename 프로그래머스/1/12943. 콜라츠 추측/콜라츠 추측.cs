c62f5d105d9060b20ad71280ce673b13656351fe
Seonggyu-Min <minsunggu705@gmail.com>
2026-07-10 00:43:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 46.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int solution(int num) {
+        int count = 0;
+        long n = num;
+
+        while (n != 1)
+        {
+            if (count == 500) break;
+
+            if (n % 2 == 0) n /= 2;
+            else { n *= 3; n += 1; }
+
+            count++;
+        }
+
+        return count == 500 ? -1 : count;
+    }
+}
