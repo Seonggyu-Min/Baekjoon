9ea72fbdc812a76cb7dec89044e4c83e781d9111
Seonggyu-Min <minsunggu705@gmail.com>
2026-07-11 04:29:03 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 7.15 ms, Memory: 48 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cs
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.cs
@@ -3,77 +3,37 @@ using System.Collections.Generic;
 
 public class Solution {
     public int solution(string message, int[,] spoiler_ranges) {
-        // 스포일러 판별용 HashSet 구성
         var spoilerIndexSet = new HashSet<int>();
+
         for (int row = 0; row < spoiler_ranges.GetLength(0); row++)
         {
-            for (int col = 0; col < spoiler_ranges.GetLength(1); col += 2)
+            int rangeStart = spoiler_ranges[row, 0];
+            int rangeEnd = spoiler_ranges[row, 1];
+
+            for (int i = rangeStart; i <= rangeEnd; i++)
             {
-                for (int i = spoiler_ranges[row, col]; i <= spoiler_ranges[row, col + 1]; i++)
-                {
-                    spoilerIndexSet.Add(i);
-                }
+                spoilerIndexSet.Add(i);
             }
         }
 
-        // 공개 단어, 스포일러 단어 구성
         var openSet = new HashSet<string>();
         var spoilerSet = new HashSet<string>();
 
-        int start = 0;
-        int end = 0;
+        int wordStart = 0;
 
-        for (int i = 0; i < message.Length; i++)
+        for (int i = 0; i <= message.Length; i++)
         {
-            if (message[i] == ' ')
-            {
-                if (i == 0)
-                {
-                    start = 1;
-                    continue;
-                }
+            bool isWordEnd = i == message.Length || message[i] == ' ';
 
-                end = i - 1;
-
-                Distribute(start, end);
-
-                start = i + 1;
-            }
-
-            // 맨 마지막일 때 처리
-            if (i == message.Length - 1)
-            {
-                end = i;
-                Distribute(start, end);
-            }
-        }
-
-        // 중요한 단어 판별
-        var importantSet = new HashSet<string>();
-
-        foreach (var word in spoilerSet)
-        {
-            // 1. 공개 단어에 있는지 판별
-            if (openSet.Contains(word))
+            if (!isWordEnd)
             {
                 continue;
             }
-            // 2. 없으면, 이미 중요한 단어에 있는지 판별
-            if (importantSet.Contains(word))
-            {
-                continue;
-            }
-            // 3. 여기까지 없으면 Set에 넣어 중복 제거 및 삽입
-            importantSet.Add(word);
-        }
 
-        return importantSet.Count;
-
-
-        void Distribute(int start, int end)
-        {
+            int wordEnd = i - 1;
             bool isSpoiler = false;
-            for (int j = start; j <= end; j++)
+
+            for (int j = wordStart; j <= wordEnd; j++)
             {
                 if (spoilerIndexSet.Contains(j))
                 {
@@ -82,14 +42,31 @@ public class Solution {
                 }
             }
 
+            string word = message.Substring(wordStart, wordEnd - wordStart + 1);
+
             if (isSpoiler)
             {
-                spoilerSet.Add(message.Substring(start, end - start + 1));
+                spoilerSet.Add(word);
             }
             else
             {
-                openSet.Add(message.Substring(start, end - start + 1));
+                openSet.Add(word);
+            }
+
+            wordStart = i + 1;
+        }
+
+        
+        int answer = 0;
+
+        foreach (string word in spoilerSet)
+        {
+            if (!openSet.Contains(word))
+            {
+                answer++;
             }
         }
+
+        return answer;
     }
 }
