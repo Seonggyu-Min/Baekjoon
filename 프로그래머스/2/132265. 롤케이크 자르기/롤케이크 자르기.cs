88ce1d23fe6d43d841f1c88ce87c5b7bc434b8e8
Seonggyu-Min <minsunggu705@gmail.com>
2026-07-31 00:37:56 +0900

    [level 2] Title: 롤케이크 자르기, Time: 51.12 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(int[] topping) {
+        // n <= 1_000_000
+        int answer = 0;
+        
+        // dict 두 개로 분리
+        var left = new Dictionary<int/*topping*/, int/*quantity*/>();
+        var right = new Dictionary<int/*topping*/, int/*quantity*/>();
+
+        // 오른쪽 전체 초기화
+        for (int i = 0; i < topping.Length; i++)
+        {
+            var key = topping[i];
+
+            if (right.TryGetValue(key, out var count))
+            {
+                right[key] = count + 1;
+            }
+            else
+            {
+                right[key] = 1;
+            }
+        }
+
+        for (int i = 0; i < topping.Length; i++)
+        {
+            var key = topping[i];
+
+            if (left.TryGetValue(topping[i], out var leftCount))
+            {
+                left[key] = leftCount + 1;
+            }
+            else
+            {
+                left[key] = 1;
+            }
+
+            if (right.TryGetValue(topping[i], out var rightCount))
+            {
+                if (1 < rightCount)
+                {
+                    right[key] = rightCount - 1;
+                }
+                else
+                {
+                    right.Remove(key);
+                }
+            }
+
+            if (left.Count == right.Count)
+            {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
