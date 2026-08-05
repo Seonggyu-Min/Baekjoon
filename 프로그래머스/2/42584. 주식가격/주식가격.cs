322aab075a8c7dad86e9050d7e965e8cd54ef1da
Seonggyu-Min <minsunggu705@gmail.com>
2026-08-06 01:04:02 +0900

    [level 2] Title: 주식가격, Time: 3.66 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cs
+++ 프로그래머스/2/42584. 주식가격/주식가격.cs
@@ -9,14 +9,14 @@ public class Solution {
         // 뒤에서 부터 순회
         // 비교하고 있는 i보다 stack에 큰 것이 있으면 Pop
 
-        // 다만 가격이 언제 떨어지는지도 확인해야되므로 튜플로 배열 인덱스, 가격 기록
-        var stack = new Stack<(int index, int price)>();
+        // 가격이 언제 떨어지는지 Stack에 기록
+        var stack = new Stack<int/*index*/>();
         var result = new int[prices.Length];
 
         for (int i = prices.Length - 1; i >= 0; i--)
         {
             // 현재 가격보다 크거나 같은 가격이 있는지 확인
-            while (stack.TryPeek(out var t) && prices[i] <= t.price)
+            while (stack.TryPeek(out var t) && prices[i] <= prices[t])
             {
                 stack.Pop();
             }
@@ -24,14 +24,14 @@ public class Solution {
             // Peek 해서 나온 것이 현재 i보다 작은 것이 보장됨
             if (stack.TryPeek(out var p))
             {
-                result[i] = p.index - i;
+                result[i] = p - i;
             }
             else
             {
                 result[i] = (prices.Length - 1) - i;
             }
 
-            stack.Push((i, prices[i]));
+            stack.Push(i);
         }
 
         return result;
