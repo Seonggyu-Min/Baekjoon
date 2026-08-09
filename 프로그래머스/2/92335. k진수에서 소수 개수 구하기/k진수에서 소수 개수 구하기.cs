d856d24e42b86d6837710ff0fcca8f0630ce9e2f
Seonggyu-Min <minsunggu705@gmail.com>
2026-08-10 00:26:42 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 6.63 ms, Memory: 48.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public int solution(int n, int k) {
+        // n <= 1,000,000
+        // 3 <= k <= 10
+        // 3진수의 경우 13자리 수 까지 씀
+
+        // k진수 변환
+        int num = n;
+        var sb = new StringBuilder();
+        while (num != 0)
+        {
+            sb.Append(num % k);
+            num /= k;
+        }
+
+        for (int i = 0, j = sb.Length - 1; i < j; i++, j--)
+        {
+            (sb[i], sb[j]) = (sb[j], sb[i]);
+        }
+
+        // 0을 제거하고 해당 단위로 분리
+        var converted = sb.ToString();
+        var splited = converted.Split('0', StringSplitOptions.RemoveEmptyEntries);
+
+        // 분리된 수가 소수인지 검사
+        int count = 0;
+        foreach (var i in splited)
+        {
+            if (IsPrime(long.Parse(i)))
+            {
+                count++;
+            }
+        }
+
+        return count;
+
+        // O(log(n))
+        bool IsPrime(long n)
+        {
+            if (n < 2) return false;
+
+            for (long i = 2; i <= n / i; i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
