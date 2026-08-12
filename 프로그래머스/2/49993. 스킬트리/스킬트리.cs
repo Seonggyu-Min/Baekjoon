29cbb5600712022898204f03cc192e56ed16aeb8
Seonggyu-Min <minsunggu705@gmail.com>
2026-08-13 00:27:30 +0900

    [level 2] Title: 스킬트리, Time: 1.12 ms, Memory: 46.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int solution(string skill, string[] skill_trees) {
+        // 1 <= skill.Length <= 26
+        // 2 <= skill_trees.Length <= 20
+        // 2 <= skill_trees 원소의 Length <= 26
+
+        // e.g. 
+        // "CBD"
+        // ["BACDE", "CBADF", "AECB", "BDA"]
+
+        var set = new HashSet<char>(skill);
+        int count = 0;
+
+        // 2 <= n <= 520
+        foreach (var userSkillTree in skill_trees)
+        {
+            var queue = new Queue<char>(skill);
+
+            bool isValid = true;
+            foreach (var userSkill in userSkillTree)
+            {
+                // 스킬트리의 스킬이 선행 스킬에 존재하는지 set으로 확인
+                if (set.Contains(userSkill))
+                {
+                    // 존재한다면 Queue.Dequeue == 스킬 확인
+
+                    var dequeue = queue.Dequeue();
+
+                    if (userSkill == dequeue)
+                    {
+                        queue.Enqueue(dequeue);
+                    }
+                    // 유효하지 않은 스킬트리
+                    else
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                // 선행 스킬에 없는 스킬이면 무시
+            }
+
+            if (isValid)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
